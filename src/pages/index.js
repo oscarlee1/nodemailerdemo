35066946b7d412e7f4a687f2113c6a4e20e1e6ac
Oscar Lee <oscarlee@Oscars-MacBook-Pro.local>
2023-05-27 16:21:20 -0400

    Lorem ipsum

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,6 +42,9 @@ export default function Home() {
     } else {
       const res = await fetch(`${ process.env.NEXT_PUBLIC_BASE_URL }/api/nodemailer`, {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
         body: JSON.stringify({ name: name, email: email, subject:subject, message: message })
       })
   
