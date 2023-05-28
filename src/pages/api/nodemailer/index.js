2d0c92addd0af3fa47300c10e3ea31cac78de5ba
Oscar Lee <oscarlee@Oscars-MacBook-Pro.local>
2023-05-28 13:31:12 -0400

    Lorem ipsum

--- src/pages/api/nodemailer/index.js
+++ src/pages/api/nodemailer/index.js
@@ -4,9 +4,18 @@ import { transporter } from "../../../lib/nodemailer"
 export default async function handler(req, res) {
   const { method } = req
 
+  try {
+    res.json({ success: true, method: method, body: req.body })
+  } catch(err) {
+    res.json({ success: false, message: err.message })
+  }
+
+  /*
   switch(method) {
     case "POST":
       const requestBody = JSON.parse(req.body)
+
+      console.log(requestBody)
 
       const from = process.env.EMAIL
       const to = requestBody.email
@@ -28,4 +37,5 @@ export default async function handler(req, res) {
       res.json({ success: false })
       break
   }
+  */
 }
